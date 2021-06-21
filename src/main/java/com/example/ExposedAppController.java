676123cd2ff3d9b4e8b5d50b0927d2b55e61cb49
Chris Laprun <metacosm@gmail.com>
2021-06-21 22:15:18 +0200

    feat: initial controller implementation

--- src/main/java/com/example/ExposedAppController.java
+++ src/main/java/com/example/ExposedAppController.java
@@ -1,12 +1,24 @@
 package com.example;
 
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
+import io.fabric8.kubernetes.api.model.networking.v1.IngressBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.*;
 import io.javaoperatorsdk.operator.api.Context;
 import io.javaoperatorsdk.operator.processing.event.EventSourceManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Controller
+import java.util.Map;
+
+@Controller(name = "devnation", finalizerName = Controller.NO_FINALIZER, namespaces = Controller.WATCH_CURRENT_NAMESPACE)
 public class ExposedAppController implements ResourceController<ExposedApp> {
+
+    private static final Logger log = LoggerFactory.getLogger(ExposedAppController.class);
+    public static final String APP_LABEL = "app.kubernetes.io/name";
 
     private final KubernetesClient client;
 
@@ -24,9 +36,80 @@ public class ExposedAppController implements ResourceController<ExposedApp> {
     @Override
     public UpdateControl<ExposedApp> createOrUpdateResource(
         ExposedApp resource, Context<ExposedApp> context) {
-        // TODO: fill in logic
+            final var spec = resource.getSpec();
+            final var name = resource.getMetadata().getName();
+            final var imageRef = spec.getImageRef();
+            log.info("Exposing {} application from image {}", name, imageRef);
 
-        return UpdateControl.noUpdate();
+            // create Deployment
+            var labels = Map.of(APP_LABEL, name);
+            final var deployment = client.apps().deployments().createOrReplace(new DeploymentBuilder()
+                    .withMetadata(createMetadata(resource, labels))
+                    .withNewSpec()
+                        .withNewSelector().withMatchLabels(labels).endSelector()
+                        .withNewTemplate()
+                            .withNewMetadata().withLabels(labels).endMetadata()
+                            .withNewSpec()
+                                .addNewContainer()
+                                    .withName(name).withImage(imageRef)
+                                    .addNewPort()
+                                        .withName("http").withProtocol("TCP").withContainerPort(8080)
+                                    .endPort()
+                                .endContainer()
+                            .endSpec()
+                        .endTemplate()
+                    .endSpec()
+                    .build());
+            log.info("Deployment {} handled", deployment.getMetadata().getName());
+
+            // create Service
+            final var service = client.services().createOrReplace(new ServiceBuilder()
+                    .withMetadata(createMetadata(resource, labels))
+                    .withNewSpec()
+                        .addNewPort()
+                            .withName("http")
+                            .withPort(8080)
+                            .withNewTargetPort().withIntVal(8080).endTargetPort()
+                        .endPort()
+                        .withSelector(labels)
+                        .withType("ClusterIP")
+                    .endSpec()
+                    .build());
+            log.info("Service {} handled", service.getMetadata().getName());
+
+            // create Ingress
+            final var metadata = createMetadata(resource, labels);
+            metadata.setAnnotations(Map.of("nginx.ingress.kubernetes.io/rewrite-target", "/"));
+            final var ingress = client.network().v1().ingresses().createOrReplace(new IngressBuilder()
+                    .withMetadata(metadata)
+                    .withNewSpec()
+                        .addNewRule()
+                            .withNewHttp()
+                                .addNewPath()
+                                    .withPath("/")
+                                    .withPathType("Prefix")
+                                    .withNewBackend()
+                                        .withNewService()
+                                            .withName(metadata.getName())
+                                            .withNewPort().withNumber(8080).endPort()
+                                        .endService()
+                                    .endBackend()
+                                .endPath()
+                            .endHttp()
+                        .endRule()
+                    .endSpec()
+                    .build());
+            log.info("Ingress {} handled", ingress.getMetadata().getName());
+
+            return UpdateControl.noUpdate();
+    }
+
+    private ObjectMeta createMetadata(ExposedApp resource, Map<String, String> labels) {
+        final var metadata = resource.getMetadata();
+        return new ObjectMetaBuilder()
+                .withName(metadata.getName())
+                .withLabels(labels)
+                .build();
     }
 }
 
--- src/main/java/com/example/ExposedAppSpec.java
+++ src/main/java/com/example/ExposedAppSpec.java
@@ -3,4 +3,13 @@ package com.example;
 public class ExposedAppSpec {
 
     // Add Spec information here
+    private String imageRef;
+
+    public String getImageRef() {
+        return imageRef;
+    }
+
+    public void setImageRef(String imageRef) {
+        this.imageRef = imageRef;
+    }
 }
